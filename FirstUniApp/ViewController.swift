c3894f8b2c7e552e63457a65adf8baa93397760c
Stanimir Hristov <stanimir.hristov@mobisystems.com>
2018-10-11 19:44:49 +0300

    Initial Commit

---
+++ FirstUniApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstUniApp
+//
+//  Created by Stanimir Hristov on 10/11/18.
+//  Copyright © 2018 Stanimir Hristov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
